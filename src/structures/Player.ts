d66fd855c4289cbda9a8a24ea0ef29f054430841
tomato6966 <chris.pre03@gmail.com>
2023-08-15 10:54:52 +0200

    allow player#node option to work

--- src/structures/Player.ts
+++ src/structures/Player.ts
@@ -124,7 +124,8 @@ export class Player {
         this.textChannelId = this.options.textChannelId || null;
 
         this.node = typeof this.options.node === "string" ? this.LavalinkManager.nodeManager.nodes.get(this.options.node) : this.options.node;
-        if(!this.node || typeof this.node.request !== "function") this.node = this.LavalinkManager.nodeManager.leastUsedNodes.filter(v => options.vcRegion ? v.options?.regions?.includes(options.vcRegion) : true)[0] || this.LavalinkManager.nodeManager.leastUsedNodes[0] || null;
+        if(!this.node || typeof this.node?.request !== "function") this.node = this.LavalinkManager.nodeManager.leastUsedNodes.filter(v => options.vcRegion ? v.options?.regions?.includes(options.vcRegion) : true)[0] || this.LavalinkManager.nodeManager.leastUsedNodes[0] || null;
+       
         if(!this.node) throw new Error("No available Node was found, please add a LavalinkNode to the Manager via Manager.NodeManager#createNode")
        
         if(this.LavalinkManager.options.playerOptions.volumeDecrementer) this.volume *= this.LavalinkManager.options.playerOptions.volumeDecrementer;
