fdfb9eb74899301023114a2b4de109c1ab03fbc0
tomato6966 <chris.pre03@gmail.com>
2023-08-19 10:50:42 +0200

    Applied all necessary interfaces comments

--- src/structures/Filters.ts
+++ src/structures/Filters.ts
@@ -1,14 +1,15 @@
 import { Player } from "./Player";
+import { FloatNumber, IntegerNumber } from "./Utils";
 
-export type AudioOutputs = "mono" | "stereo" | "left" | "right";
-
-export interface FilterManager {
-    player: Player;
-}
-
+/**
+ * The FilterManager for each player
+ */
 export class FilterManager {
+    /** The Equalizer bands currently applied to the Lavalink Server */
     public equalizerBands: EQBand[] = [];
+    /** Private Util for the instaFix Filters option */
     public filterUpdatedState: number = 0;
+    /** All "Active" / "disabled" Player Filters */
     public filters: PlayerFilters = {
         volume: false,
         vaporwave: false,
@@ -23,6 +24,7 @@ export class FilterManager {
         lowPass: false,
         audioOutput: "stereo",
     }
+    /** The Filter Data sent to Lavalink, only if the filter is enabled (ofc.) */
     public data: FilterData = {
         lowPass: {
             smoothing: 0
@@ -69,10 +71,17 @@ export class FilterManager {
             scale: 1
         }*/
     }
+    /** The Player assigned to this Filter Manager */
+    public player: Player;
+    /** The Constructor for the FilterManager */
     constructor(player: Player) {
+        /** Assign the player to the filter manager */
         this.player = player;
     }
-    // function to update all filters at ONCE (and eqs)
+    
+    /**
+     * Apply Player filters for lavalink filter sending data, if the filter is enabled / not
+     */
     async applyPlayerFilters() {
         const sendData = { ...this.data } as LavalinkFilterData & { equalizer: EQBand[] };
 
@@ -189,19 +198,25 @@ export class FilterManager {
         return this.filters;
     }
 
-    public async setVolume(volume:number) {
-        if(volume < 0 || volume > 5) throw new SyntaxError("Volume-Filter must be between 0 and 5");
-        
+    /**
+     * Set the Filter Volume
+     * @param volume 
+     * @returns 
+     */
+    public async setVolume(volume: number) {
+        if (volume < 0 || volume > 5) throw new SyntaxError("Volume-Filter must be between 0 and 5");
+
         this.data.volume = volume;
-        
+
         await this.applyPlayerFilters();
-        
+
         return this.filters.volume;
     }
 
     /**
      * Set the AudioOutput Filter 
      * @param type 
+     * @returns
      */
     public async setAudioOutput(type: AudioOutputs): Promise<AudioOutputs> {
         if (this.player.node.info && !this.player.node.info?.filters?.includes("channelMix")) throw new Error("Node#Info#filters does not include the 'channelMix' Filter (Node has it not enable)")
@@ -434,11 +449,11 @@ export class FilterManager {
         this.filters.custom = !this.filters.nightcore && !this.filters.vaporwave && Object.values(this.data.timescale).some(d => d !== 1);
         return this.filters.custom;
     }
-      /**
-     * Sets the players equalizer band on-top of the existing ones.
-     * @param bands
-     */
-    public async setEQ(bands: EQBand|EQBand[]): Promise<this> {
+    /**
+   * Sets the players equalizer band on-top of the existing ones.
+   * @param bands
+   */
+    public async setEQ(bands: EQBand | EQBand[]): Promise<this> {
         if (!Array.isArray(bands)) bands = [bands];
 
         if (!bands.length || !bands.every((band) => JSON.stringify(Object.keys(band).sort()) === '["band","gain"]')) throw new TypeError("Bands must be a non-empty object array containing 'band' and 'gain' properties.");
@@ -446,29 +461,34 @@ export class FilterManager {
         for (const { band, gain } of bands) this.equalizerBands[band] = { band, gain };
 
         if (!this.player.node.sessionId) throw new Error("The Lavalink-Node is either not ready or not up to date");
-        
+
         const now = performance.now();
 
         await this.player.node.updatePlayer({
             guildId: this.player.guildId,
             playerOptions: {
-            filters: { equalizer: this.equalizerBands } }
+                filters: { equalizer: this.equalizerBands }
+            }
         });
 
         this.player.ping.lavalink = Math.round((performance.now() - now) / 10) / 100;
-        
+
         if (this.player.options.instaUpdateFiltersFix === true) this.filterUpdatedState = 1;
-        
+
         return this;
     }
 
     /** Clears the equalizer bands. */
     public async clearEQ(): Promise<this> {
-        return this.setEQ(new Array(15).fill(0.0).map((gain, band) => ({ band, gain })));
+        return this.setEQ(new Array(15).fill(0.0).map((gain:FloatNumber, band:IntegerNumber) => ({ band, gain })));
     }
 }
 
-export const audioOutputsData = {
+/** The Audio Outputs type */
+export type AudioOutputs = "mono" | "stereo" | "left" | "right";
+
+/**  The audio Outputs Data map declaration */
+export const audioOutputsData:Record<AudioOutputs, ChannelMixFilter> = {
     mono: { // totalLeft: 1, totalRight: 1
         leftToLeft: 0.5, //each channel should in total 0 | 1, 0 === off, 1 === on, 0.5+0.5 === 1
         leftToRight: 0.5,
@@ -494,6 +514,8 @@ export const audioOutputsData = {
         rightToRight: 1,
     },
 }
+
+/** The "active" / "disabled" Player Filters */
 export interface PlayerFilters {
     /** Sets nightcore to false, and vaporwave to false */
     custom: boolean;
@@ -501,47 +523,96 @@ export interface PlayerFilters {
     nightcore: boolean;
     /** Sets custom to false, and nightcore to false */
     vaporwave: boolean;
-    /** only with the custom lavalink filter plugin */
-    echo: boolean;
-    /** only with the custom lavalink filter plugin */
-    reverb: boolean;
+    /** If rotation filter is enabled / not */
     rotation: boolean;
+    /** if karaoke filter is enabled / not */
     karaoke: boolean;
+    /** if tremolo filter is enabled / not */
     tremolo: boolean;
+    /** if vibrato filter is enabled / not */
     vibrato: boolean;
+    /* if lowpass filter is enabled / not */
     lowPass: boolean;
     /** audio Output (default stereo, mono sounds the fullest and best for not-stereo tracks) */
     audioOutput: AudioOutputs;
     /** Lavalink Volume FILTER (not player Volume, think of it as a gain booster) */
     volume: boolean;
+    /** only with the custom lavalink filter plugin */
+    echo: boolean;
+    /** only with the custom lavalink filter plugin */
+    reverb: boolean;
 }
 
+/** 
+ * There are 15 bands (0-14) that can be changed. 
+ * "gain" is the multiplier for the given band. 
+ * The default value is 0.
+ *  Valid values range from -0.25 to 1.0, where -0.25 means the given band is completely muted, and 0.25 means it is doubled. 
+ * Modifying the gain could also change the volume of the output.
+ */
 export interface EQBand {
-    band: number;
-    gain: number;
+    /** On what band position (0-14) it should work */
+    band: IntegerNumber;
+    /** The gain (-0.25 to 1.0) */
+    gain: FloatNumber;
 }
+/**
+ * Uses equalization to eliminate part of a band, usually targeting vocals.
+ */
 export interface KaraokeFilter {
+    /** The level (0 to 1.0 where 0.0 is no effect and 1.0 is full effect) */
     level?: number;
+    /** The mono level (0 to 1.0 where 0.0 is no effect and 1.0 is full effect) */
     monoLevel?: number;
+    /** The filter band (in Hz) */
     filterBand?: number;
+    /**	The filter width */
     filterWidth?: number;
 }
-
+/**
+ * Changes the speed, pitch, and rate
+ */
 export interface TimescaleFilter {
+    /** The playback speed 0.0 ≤ x */
     speed?: number;
+    /** The pitch 0.0 ≤ x */
     pitch?: number;
+    /** The rate 0.0 ≤ x */
     rate?: number;
 }
 
-export interface FreqFilter {
+/**
+ * Uses amplification to create a shuddering effect, where the volume quickly oscillates. 
+ * Demo: https://en.wikipedia.org/wiki/File:Fuse_Electronics_Tremolo_MK-III_Quick_Demo.ogv
+ */
+export interface TremoloFilter {
+    /** The frequency 0.0 < x */
     frequency?: number;
+    /** The tremolo depth 0.0 < x ≤ 1.0 */
+    depth?: number;
+}
+/**
+ * Similar to tremolo. While tremolo oscillates the volume, vibrato oscillates the pitch.
+ */
+export interface VibratoFilter {
+    /** The frequency 0.0 < x ≤ 14.0 */
+    frequency?: number;
+    /** The vibrato depth 0.0 < x ≤ 1.0 */
     depth?: number;
 }
 
+/**
+ * Rotates the sound around the stereo channels/user headphones (aka Audio Panning). 
+ * It can produce an effect similar to https://youtu.be/QB9EB8mTKcc (without the reverb).
+ */
 export interface RotationFilter {
+    /** The frequency of the audio rotating around the listener in Hz. 0.2 is similar to the example video above */
     rotationHz?: number;
 }
 
+/**
+ * Distortion effect. It can generate some pretty unique audio effects.
+ */
 export interface DistortionFilter {
     sinOffset?: number;
     sinScale?: number;
@@ -553,30 +624,53 @@ export interface DistortionFilter {
     scale?: number;
 }
 
+/**
+ * Mixes both channels (left and right), with a configurable factor on how much each channel affects the other. 
+ * With the defaults, both channels are kept independent of each other. 
+ * Setting all factors to 0.5 means both channels get the same audio.
+ */
 export interface ChannelMixFilter {
+    /** The left to left channel mix factor (0.0 ≤ x ≤ 1.0) */
     leftToLeft?: number;
+    /** The left to right channel mix factor (0.0 ≤ x ≤ 1.0) */
     leftToRight?: number;
+    /** The right to left channel mix factor (0.0 ≤ x ≤ 1.0) */
     rightToLeft?: number;
+    /** The right to right channel mix factor (0.0 ≤ x ≤ 1.0) */
     rightToRight?: number;
 }
 
+/**
+ * Higher frequencies get suppressed, while lower frequencies pass through this filter, thus the name low pass. 
+ * Any smoothing values equal to or less than 1.0 will disable the filter.
+ */
 export interface LowPassFilter {
+    /** The smoothing factor (1.0 < x) */
     smoothing?: number
 }
+/**
+ * A Plugin Filter
+ */
 export interface EchoFilter {
     delay: number
     decay: number
 }
+/**
+ * A Plugin Filter
+ */
 export interface ReverbFilter {
     delay: number
     decay: number
 }
+/**
+ * Filter Data stored in the Client and partially sent to Lavalink
+ */
 export interface FilterData {
     volume?: number;
     karaoke?: KaraokeFilter;
     timescale?: TimescaleFilter;
-    tremolo?: FreqFilter;
-    vibrato?: FreqFilter;
+    tremolo?: TremoloFilter;
+    vibrato?: VibratoFilter;
     rotation?: RotationFilter;
     distortion?: DistortionFilter;
     channelMix?: ChannelMixFilter;
@@ -584,6 +678,210 @@ export interface FilterData {
     echo: EchoFilter,
     reverb: ReverbFilter,
 }
+/** 
+ * Actual Filter Data sent to Lavalink
+ */
 export interface LavalinkFilterData extends FilterData {
     equalizer?: EQBand[];
+}
+
+
+export const EQList = {
+    /** A Bassboost Equalizer, so high it distorts the audio */
+    BassboostEarrape: [
+        { band: 0, gain: 0.6 * 0.375 },
+        { band: 1, gain: 0.67 * 0.375 },
+        { band: 2, gain: 0.67 * 0.375 },
+        { band: 3, gain: 0.4 * 0.375 },
+        { band: 4, gain: -0.5 * 0.375 },
+        { band: 5, gain: 0.15 * 0.375 },
+        { band: 6, gain: -0.45 * 0.375 },
+        { band: 7, gain: 0.23 * 0.375 },
+        { band: 8, gain: 0.35 * 0.375 },
+        { band: 9, gain: 0.45 * 0.375 },
+        { band: 10, gain: 0.55 * 0.375 },
+        { band: 11, gain: -0.6 * 0.375 },
+        { band: 12, gain: 0.55 * 0.375 },
+        { band: 13, gain: -0.5 * 0.375 },
+        { band: 14, gain: -0.75 * 0.375 },
+    ] as EQBand[],
+    /** A High and decent Bassboost Equalizer */
+    BassboostHigh: [
+        { band: 0, gain: 0.6 * 0.25 },
+        { band: 1, gain: 0.67 * 0.25 },
+        { band: 2, gain: 0.67 * 0.25 },
+        { band: 3, gain: 0.4 * 0.25 },
+        { band: 4, gain: -0.5 * 0.25 },
+        { band: 5, gain: 0.15 * 0.25 },
+        { band: 6, gain: -0.45 * 0.25 },
+        { band: 7, gain: 0.23 * 0.25 },
+        { band: 8, gain: 0.35 * 0.25 },
+        { band: 9, gain: 0.45 * 0.25 },
+        { band: 10, gain: 0.55 * 0.25 },
+        { band: 11, gain: -0.6 * 0.25 },
+        { band: 12, gain: 0.55 * 0.25 },
+        { band: 13, gain: -0.5 * 0.25 },
+        { band: 14, gain: -0.75 * 0.25 },
+    ] as EQBand[],
+    /** A decent Bassboost Equalizer */
+    BassboostMedium: [
+        { band: 0, gain: 0.6 * 0.1875 },
+        { band: 1, gain: 0.67 * 0.1875 },
+        { band: 2, gain: 0.67 * 0.1875 },
+        { band: 3, gain: 0.4 * 0.1875 },
+        { band: 4, gain: -0.5 * 0.1875 },
+        { band: 5, gain: 0.15 * 0.1875 },
+        { band: 6, gain: -0.45 * 0.1875 },
+        { band: 7, gain: 0.23 * 0.1875 },
+        { band: 8, gain: 0.35 * 0.1875 },
+        { band: 9, gain: 0.45 * 0.1875 },
+        { band: 10, gain: 0.55 * 0.1875 },
+        { band: 11, gain: -0.6 * 0.1875 },
+        { band: 12, gain: 0.55 * 0.1875 },
+        { band: 13, gain: -0.5 * 0.1875 },
+        { band: 14, gain: -0.75 * 0.1875 },
+    ] as EQBand[],
+    /** A slight Bassboost Equalizer */
+    BassboostLow: [
+        { band: 0, gain: 0.6 * 0.125 },
+        { band: 1, gain: 0.67 * 0.125 },
+        { band: 2, gain: 0.67 * 0.125 },
+        { band: 3, gain: 0.4 * 0.125 },
+        { band: 4, gain: -0.5 * 0.125 },
+        { band: 5, gain: 0.15 * 0.125 },
+        { band: 6, gain: -0.45 * 0.125 },
+        { band: 7, gain: 0.23 * 0.125 },
+        { band: 8, gain: 0.35 * 0.125 },
+        { band: 9, gain: 0.45 * 0.125 },
+        { band: 10, gain: 0.55 * 0.125 },
+        { band: 11, gain: -0.6 * 0.125 },
+        { band: 12, gain: 0.55 * 0.125 },
+        { band: 13, gain: -0.5 * 0.125 },
+        { band: 14, gain: -0.75 * 0.125 },
+    ] as EQBand[],
+    /** Makes the Music slightly "better" */
+    BetterMusic: [
+        { band: 0, gain: 0.25 },
+        { band: 1, gain: 0.025 },
+        { band: 2, gain: 0.0125 },
+        { band: 3, gain: 0 },
+        { band: 4, gain: 0 },
+        { band: 5, gain: -0.0125 },
+        { band: 6, gain: -0.025 },
+        { band: 7, gain: -0.0175 },
+        { band: 8, gain: 0 },
+        { band: 9, gain: 0 },
+        { band: 10, gain: 0.0125 },
+        { band: 11, gain: 0.025 },
+        { band: 12, gain: 0.25 },
+        { band: 13, gain: 0.125 },
+        { band: 14, gain: 0.125 },
+    ] as EQBand[],
+    /** Makes the Music sound like rock music / sound rock music better */
+    Rock: [
+        { band: 0, gain: 0.300 },
+        { band: 1, gain: 0.250 },
+        { band: 2, gain: 0.200 },
+        { band: 3, gain: 0.100 },
+        { band: 4, gain: 0.050 },
+        { band: 5, gain: -0.050 },
+        { band: 6, gain: -0.150 },
+        { band: 7, gain: -0.200 },
+        { band: 8, gain: -0.100 },
+        { band: 9, gain: -0.050 },
+        { band: 10, gain: 0.050 },
+        { band: 11, gain: 0.100 },
+        { band: 12, gain: 0.200 },
+        { band: 13, gain: 0.250 },
+        { band: 14, gain: 0.300 },
+    ] as EQBand[],
+    /** Makes the Music sound like Classic music / sound Classic music better */
+    Classic: [
+        { band: 0, gain: 0.375 },
+        { band: 1, gain: 0.350 },
+        { band: 2, gain: 0.125 },
+        { band: 3, gain: 0 },
+        { band: 4, gain: 0 },
+        { band: 5, gain: 0.125 },
+        { band: 6, gain: 0.550 },
+        { band: 7, gain: 0.050 },
+        { band: 8, gain: 0.125 },
+        { band: 9, gain: 0.250 },
+        { band: 10, gain: 0.200 },
+        { band: 11, gain: 0.250 },
+        { band: 12, gain: 0.300 },
+        { band: 13, gain: 0.250 },
+        { band: 14, gain: 0.300 },
+    ] as EQBand[],
+    /** Makes the Music sound like Pop music / sound Pop music better */
+    Pop: [
+        { band: 0, gain: 0.2635 },
+        { band: 1, gain: 0.22141 },
+        { band: 2, gain: -0.21141 },
+        { band: 3, gain: -0.1851 },
+        { band: 4, gain: -0.155 },
+        { band: 5, gain: 0.21141 },
+        { band: 6, gain: 0.22456 },
+        { band: 7, gain: 0.237 },
+        { band: 8, gain: 0.237 },
+        { band: 9, gain: 0.237 },
+        { band: 10, gain: -0.05 },
+        { band: 11, gain: -0.116 },
+        { band: 12, gain: 0.192 },
+        { band: 13, gain: 0 },
+    ] as EQBand[],
+    /** Makes the Music sound like Electronic music / sound Electronic music better */
+    Electronic: [
+        { band: 0, gain: 0.375 },
+        { band: 1, gain: 0.350 },
+        { band: 2, gain: 0.125 },
+        { band: 3, gain: 0 },
+        { band: 4, gain: 0 },
+        { band: 5, gain: -0.125 },
+        { band: 6, gain: -0.125 },
+        { band: 7, gain: 0 },
+        { band: 8, gain: 0.25 },
+        { band: 9, gain: 0.125 },
+        { band: 10, gain: 0.15 },
+        { band: 11, gain: 0.2 },
+        { band: 12, gain: 0.250 },
+        { band: 13, gain: 0.350 },
+        { band: 14, gain: 0.400 },
+    ] as EQBand[],
+    /** Boosts all Bands slightly for louder and fuller sound */
+    FullSound: [
+        { band: 0, gain: 0.25 + 0.375 },
+        { band: 1, gain: 0.25 + 0.025 },
+        { band: 2, gain: 0.25 + 0.0125 },
+        { band: 3, gain: 0.25 + 0 },
+        { band: 4, gain: 0.25 + 0 },
+        { band: 5, gain: 0.25 + -0.0125 },
+        { band: 6, gain: 0.25 + -0.025 },
+        { band: 7, gain: 0.25 + -0.0175 },
+        { band: 8, gain: 0.25 + 0 },
+        { band: 9, gain: 0.25 + 0 },
+        { band: 10, gain: 0.25 + 0.0125 },
+        { band: 11, gain: 0.25 + 0.025 },
+        { band: 12, gain: 0.25 + 0.375 },
+        { band: 13, gain: 0.25 + 0.125 },
+        { band: 14, gain: 0.25 + 0.125 },
+    ] as EQBand[],
+    /** Boosts basses + lower highs for a pro gaming sound */
+    Gaming: [
+        { band: 0, gain: 0.350 },
+        { band: 1, gain: 0.300 },
+        { band: 2, gain: 0.250 },
+        { band: 3, gain: 0.200 },
+        { band: 4, gain: 0.150 },
+        { band: 5, gain: 0.100 },
+        { band: 6, gain: 0.050 },
+        { band: 7, gain: -0.0 },
+        { band: 8, gain: -0.050 },
+        { band: 9, gain: -0.100 },
+        { band: 10, gain: -0.150 },
+        { band: 11, gain: -0.200 },
+        { band: 12, gain: -0.250 },
+        { band: 13, gain: -0.300 },
+        { band: 14, gain: -0.350 },
+    ] as EQBand[],
 }
