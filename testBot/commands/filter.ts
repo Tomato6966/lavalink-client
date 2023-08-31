d4561eef8b9a86e65ebdb84eb32348196c704c71
tomato6966 <chris.pre03@gmail.com>
2023-08-31 21:07:34 +0200

    example for echo + reverb filters [soon]

--- testBot/commands/filter.ts
+++ testBot/commands/filter.ts
@@ -10,12 +10,12 @@ export default {
             { name: "Nightcore", value: "nightcore" },
             { name: "Vaporwave", value: "vaporwave" },
             { name: "LowPass", value: "lowpass" },
-            { name: "Echo", value: "echo" }, // available in lavalink-filters lavalink-plugin
             { name: "Karaoke", value: "karaoke" },
             { name: "Rotation", value: "rotation" },
             { name: "Tremolo", value: "tremolo" },
             { name: "Vibrato", value: "vibrato" },
-            // { name: "Reverb", value: "" }, // reverb filter currently not available in lavalink-filters lavalink-plugin
+            { name: "Echo (N/A)", value: "echo" }, // available in lavalink-filters lavalink-plugin (currently not working in lavalink-v4)
+            { name: "Reverb (N/A)", value: "reverb" }, // available in lavalink-filters lavalink-plugin (currently not working in lavalink-v4)
         )),
     execute: async (client, interaction) => {
         if(!interaction.guildId) return;
@@ -31,12 +31,13 @@ export default {
             case "lowpass": await player.filterManager.toggleLowPass(); string = player.filterManager.filters.lowPass ? "Applied Lowpass Filter-Effect" : "Disabled Lowpass Filter-Effect"; break;
             case "nightcore": await player.filterManager.toggleNightcore(); string = player.filterManager.filters.nightcore ? "Applied Nightcore Filter-Effect, ||disabled Vaporwave (if it was active)||" : "Disabled Nightcore Filter-Effect"; break;
             case "vaporwave": await player.filterManager.toggleVaporwave(); string = player.filterManager.filters.vaporwave ? "Applied Vaporwave Filter-Effect, ||disabled Nightcore (if it was active)||" : "Disabled Vaporwave Filter-Effect"; break;
-            case "echo": await player.filterManager.toggleEcho(); string = player.filterManager.filters.echo ? "Applied Echo Filter-Effect" : "Disabled Echo Filter-Effect"; break;
             case "karaoke": await player.filterManager.toggleKaraoke(); string = player.filterManager.filters.karaoke ? "Applied Karaoke Filter-Effect" : "Disabled Karaoke Filter-Effect"; break;
             case "rotation": await player.filterManager.toggleRotation(); string = player.filterManager.filters.rotation ? "Applied Rotation Filter-Effect" : "Disabled Rotation Filter-Effect"; break;
             case "tremolo": await player.filterManager.toggleTremolo(); string = player.filterManager.filters.tremolo ? "Applied Tremolo Filter-Effect" : "Disabled Tremolo Filter-Effect"; break;
             case "vibrato": await player.filterManager.toggleVibrato(); string = player.filterManager.filters.vibrato ? "Applied Vibrato Filter-Effect" : "Disabled Vibrato Filter-Effect"; break;
-            // case "reverb": await player.filterManager.toggleReverb(); string = player.filterManager.filters.reverb ? "Applied Reverb Filter-Effect" : "Disabled Reverb Filter-Effect"; break;
+            // available in lavalink-filters lavalink-plugin (currently not working in lavalink-v4)
+            case "echo": await player.filterManager.toggleEcho(); string = player.filterManager.filters.echo ? "Applied Echo Filter-Effect" : "Disabled Echo Filter-Effect"; break;
+            case "reverb": await player.filterManager.toggleReverb(); string = player.filterManager.filters.reverb ? "Applied Reverb Filter-Effect" : "Disabled Reverb Filter-Effect"; break;
         }
         await interaction.reply({
             content: `✅ ${string}`
