8264d670cd2fe8a5ce1ce569c7c4ea0dbe100722
tomato6966 <chris.pre03@gmail.com>
2023-09-04 11:50:44 +0200

    add more util functions to reduce code size

--- src/structures/Player.ts
+++ src/structures/Player.ts
@@ -5,9 +5,9 @@ import { DefaultSources } from "./LavalinkManagerStatics";
 import { LavalinkNode } from "./Node";
 import { Queue, QueueSaver } from "./Queue";
 import { PluginInfo, Track, UnresolvedTrack } from "./Track";
-import { LavalinkPlayerVoiceOptions, SearchPlatform, SearchResult, LoadTypes, queueTrackEnd, LavaSearchType, LavaSearchResponse, LavaSrcSearchPlatformBase } from "./Utils";
+import { LavalinkPlayerVoiceOptions, SearchPlatform, SearchResult, LoadTypes, queueTrackEnd, LavaSearchType, LavaSearchResponse, LavaSrcSearchPlatformBase, LavaSearchQuery, SearchQuery } from "./Utils";
 
-type PlayerDestroyReasons = "QueueEmpty" | "NodeDestroy" | "NodeDeleted" | "LavalinkNoVoice" | "NodeReconnectFail" | "PlayerReconnectFail" | "Disconnected" | "ChannelDeleted";
+type PlayerDestroyReasons = "QueueEmpty" | "NodeDestroy" | "NodeDeleted" | "LavalinkNoVoice" | "NodeReconnectFail" | "PlayerReconnectFail" | "Disconnected" | "ChannelDeleted" | "ReconnectAllNodes" | "DisconnectAllNodes";
 export type DestroyReasonsType = PlayerDestroyReasons | string;
 
 export const DestroyReasons = {
@@ -18,7 +18,9 @@ export const DestroyReasons = {
     NodeReconnectFail: "NodeReconnectFail",
     Disconnected: "Disconnected",
     PlayerReconnectFail: "PlayerReconnectFail",
-    ChannelDeleted: "ChannelDeleted"
+    ChannelDeleted: "ChannelDeleted",
+    DisconnectAllNodes: "DisconnectAllNodes",
+    ReconnectAllNodes: "ReconnectAllNodes"
 } as Record<PlayerDestroyReasons, PlayerDestroyReasons>
 
 export interface PlayerJson {
@@ -298,90 +300,24 @@ export class Player {
         return;
     }
 
-    async lavaSearch(query: { query: string, source: LavaSrcSearchPlatformBase, types?: LavaSearchType[] }, requestUser: unknown) {
-        // transform the query object
-        const Query = { 
-            query: typeof query === "string" ? query : query.query, 
-            types: query.types ? ["track", "playlist", "artist", "album", "text"].filter(v => query.types?.find(x => x.toLowerCase().startsWith(v))) : ["track", "playlist", "artist", "album", "text"],
-            source: DefaultSources[(typeof query === "string" ? undefined : query.source?.trim?.()?.toLowerCase?.()) ?? this.LavalinkManager.options.playerOptions.defaultSearchPlatform.toLowerCase()] ?? (typeof query === "string" ? undefined : query.source?.trim?.()?.toLowerCase?.()) ?? this.LavalinkManager.options.playerOptions.defaultSearchPlatform 
-        }
-
-        // if user does player.search("ytsearch:Hello")
-        const foundSource = Object.keys(DefaultSources).find(source => Query.query.toLowerCase().startsWith(`${source}:`.toLowerCase()))?.trim?.()?.toLowerCase?.() as SearchPlatform | undefined;
-        if(foundSource && DefaultSources[foundSource]){
-            Query.source = DefaultSources[foundSource]; // set the source to ytsearch:
-            Query.query = Query.query.slice(`${foundSource}:`.length, Query.query.length); // remove ytsearch: from the query
-        }
-
-        if(Query.source) this.LavalinkManager.utils.validateSourceString(this.node, Query.source);
-        if(!["spsearch", "sprec", "amsearch", "dzsearch", "dzisrc", "ytmsearch", "ytsearch"].includes(Query.source)) throw new SyntaxError(`Query.source must be a source from LavaSrc: "spsearch" | "sprec" | "amsearch" | "dzsearch" | "dzisrc" | "ytmsearch" | "ytsearch"`)
-        
-        if(/^https?:\/\//.test(Query.query)) return await this.search({ query: Query.query, source: Query.source }, requestUser);
-
-
-        if(!this.node.info.plugins.find(v => v.name === "lavasearch-plugin")) throw new RangeError(`there is no lavasearch-plugin available in the lavalink node: ${this.node.id}`);
-
-        const res = await this.node.request(`/loadsearch?query=${Query.source ? `${Query.source}:` : ""}${encodeURIComponent(Query.query)}${Query.types?.length ? `&types=${Query.types.join(",")}`: ""}`) as LavaSearchResponse;
-        return {
-            tracks: res.tracks?.map(v => this.LavalinkManager.utils.buildTrack(v, requestUser)) || [],
-            albums: res.albums?.map(v => ({info: v.info, pluginInfo: (v as unknown as { plugin: unknown })?.plugin || v.pluginInfo, tracks: v.tracks.map(v => this.LavalinkManager.utils.buildTrack(v, requestUser)) })) || [],
-            artists: res.artists?.map(v => ({info: v.info, pluginInfo: (v as unknown as { plugin: unknown })?.plugin || v.pluginInfo, tracks: v.tracks.map(v => this.LavalinkManager.utils.buildTrack(v, requestUser)) })) || [],
-            playlists: res.playlists?.map(v => ({info: v.info, pluginInfo: (v as unknown as { plugin: unknown })?.plugin || v.pluginInfo, tracks: v.tracks.map(v => this.LavalinkManager.utils.buildTrack(v, requestUser)) })) || [],
-            texts: res.texts?.map(v => ({text: v.text, pluginInfo: (v as unknown as { plugin: unknown })?.plugin || v.pluginInfo })) || [],
-            pluginInfo: res.pluginInfo || (res as unknown as { plugin: unknown })?.plugin
-        } as LavaSearchResponse
+    async lavaSearch(query:LavaSearchQuery, requestUser: unknown) {
+        return this.node.lavaSearch(query, requestUser);
     }
+
     /**
      * 
      * @param query Query for your data
      * @param requestUser 
      */
-    async search(query: { query: string, source?: SearchPlatform } | string, requestUser: unknown) {
-        // transform the query object
-        const Query = { 
-            query: typeof query === "string" ? query : query.query, 
-            source: DefaultSources[(typeof query === "string" ? undefined : query.source?.trim?.()?.toLowerCase?.()) ?? this.LavalinkManager.options.playerOptions.defaultSearchPlatform.toLowerCase()] ?? (typeof query === "string" ? undefined : query.source?.trim?.()?.toLowerCase?.()) ?? this.LavalinkManager.options.playerOptions.defaultSearchPlatform 
-        }
+    async search(query: SearchQuery, requestUser: unknown) {
+        const Query = this.LavalinkManager.utils.transformQuery(query);
         
-        // if user does player.search("ytsearch:Hello")
-        const foundSource = Object.keys(DefaultSources).find(source => Query.query?.toLowerCase?.()?.startsWith(`${source}:`.toLowerCase()))?.trim?.()?.toLowerCase?.() as SearchPlatform | undefined;
-        if(foundSource && DefaultSources[foundSource]){
-            Query.source = DefaultSources[foundSource]; // set the source to ytsearch:
-            Query.query = Query.query.slice(`${foundSource}:`.length, Query.query.length); // remove ytsearch: from the query
-        }
         if(/^https?:\/\//.test(Query.query)) this.LavalinkManager.utils.validateQueryString(this.node, Query.source);
         else if(Query.source) this.LavalinkManager.utils.validateSourceString(this.node, Query.source);
-
         
-        if(["bcsearch", "bandcamp"].includes(Query.source)) {
-            return await bandCampSearch(this, Query.query, requestUser);
-        }
+        if(["bcsearch", "bandcamp"].includes(Query.source)) return await bandCampSearch(this, Query.query, requestUser);
 
-        // ftts query parameters: ?voice=Olivia&audio_format=ogg_opus&translate=False&silence=1000&speed=1.0 | example raw get query: https://api.flowery.pw/v1/tts?voice=Olivia&audio_format=ogg_opus&translate=False&silence=0&speed=1.0&text=Hello%20World
-        // request the data 
-        const res = await this.node.request(`/loadtracks?identifier=${!/^https?:\/\//.test(Query.query) ? `${Query.source}:${Query.source === "ftts" ? "//" : ""}` : ""}${encodeURIComponent(Query.query)}`) as {
-            loadType: LoadTypes,
-            data: any,
-            pluginInfo: PluginInfo,
-        };
-
-        // transform the data which can be Error, Track or Track[] to enfore [Track] 
-        const resTracks = res.loadType === "playlist" ? res.data?.tracks : res.loadType === "track" ? [res.data] : res.loadType === "search" ? Array.isArray(res.data) ? res.data : [res.data] : [];
-
-        return {
-            loadType: res.loadType,
-            exception: res.loadType === "error" ? res.data : null,
-            pluginInfo: res.pluginInfo || {},
-            playlist: res.loadType === "playlist" ? {
-                title: res.data.info?.name || res.data.pluginInfo?.name || null,
-                author: res.data.info?.author || res.data.pluginInfo?.author || null,
-                thumbnail: (res.data.info?.artworkUrl) || (res.data.pluginInfo?.artworkUrl) || ((typeof res.data?.info?.selectedTrack !== "number" || res.data?.info?.selectedTrack === -1) ? null : resTracks[res.data?.info?.selectedTrack] ? (resTracks[res.data?.info?.selectedTrack]?.info?.artworkUrl || resTracks[res.data?.info?.selectedTrack]?.info?.pluginInfo?.artworkUrl) : null) || null,
-                uri: res.data.info?.url || res.data.info?.uri || res.data.info?.link || res.data.pluginInfo?.url || res.data.pluginInfo?.uri || res.data.pluginInfo?.link || null,
-                selectedTrack: typeof res.data?.info?.selectedTrack !== "number" || res.data?.info?.selectedTrack === -1 ? null : resTracks[res.data?.info?.selectedTrack] ? this.LavalinkManager.utils.buildTrack(resTracks[res.data?.info?.selectedTrack], requestUser) : null,
-                duration: resTracks.length ? resTracks.reduce((acc, cur) => acc + (cur?.info?.duration || 0), 0) : 0,
-            } : null,
-            tracks: resTracks.length ? resTracks.map(t => this.LavalinkManager.utils.buildTrack(t, requestUser)) : []
-        } as SearchResult;
+        return this.node.search(Query, requestUser);
     }
 
     /**
