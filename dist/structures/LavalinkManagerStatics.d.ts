05625eba22c4b4e43d088f616560a25777c40f04
tomato6966 <chris.pre03@gmail.com>
2024-09-12 19:36:22 +0200

    v2.3.1 (fix dist and types)

--- dist/structures/LavalinkManagerStatics.d.ts
+++
@@ -1,3 +0,0 @@
-import { LavalinkSearchPlatform, SearchPlatform, SourcesRegex } from "./Utils";
-export declare const DEFAULT_SOURCES: Record<SearchPlatform, LavalinkSearchPlatform>;
-export declare const REGEXES: Record<SourcesRegex, RegExp>;
