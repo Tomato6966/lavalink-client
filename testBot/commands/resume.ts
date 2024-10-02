e92953892439a6721bda3c6f6e008aa42e2afa66
tomato6966 <chris.pre03@gmail.com>
2024-10-02 18:06:25 +0200

    Added new sample commands to the testbot

---
+++ testBot/commands/resume.ts
@@ -0,0 +1,23 @@
+import { GuildMember, SlashCommandBuilder } from "discord.js";
+
+import { Command } from "../types/Client";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("resume").setDescription("Resume the player"),
+    execute: async (client, interaction) => {
+        if(!interaction.guildId) return;
+        const vcId = (interaction.member as GuildMember)?.voice?.channelId;
+        const player = client.lavalink.getPlayer(interaction.guildId);
+        if(!player) return interaction.reply({ ephemeral: true, content: "I'm not connected" });
+        if(!vcId) return interaction.reply({ ephemeral: true, content: "Join a Voice Channel "});
+        if(player.voiceChannelId !== vcId) return interaction.reply({ ephemeral: true, content: "You need to be in my Voice Channel" })
+        if(!player.paused) return interaction.reply({ ephemeral: true, content: "Not paused" })
+
+        await player.resume();
+
+        await interaction.reply({
+            ephemeral: true, content: `Resumed the player`
+        });
+    }
+} as Command;
--- testBot/commands/stop.ts
+++ testBot/commands/stop.ts
@@ -1,15 +1,16 @@
 import { GuildMember, SlashCommandBuilder } from "discord.js";
+
 import { Command } from "../types/Client";
 
 export default {
     data: new SlashCommandBuilder()
-        .setName("stop").setDescription("Stops the player"),
+        .setName("stop").setDescription("Stops the player & leaves the voice"),
     execute: async (client, interaction) => {
         if(!interaction.guildId) return;
         const vcId = (interaction.member as GuildMember)?.voice?.channelId;
         const player = client.lavalink.getPlayer(interaction.guildId);
         if(!player) return interaction.reply({ ephemeral: true, content: "I'm not connected" });
-        
+
         // example to apply a filter!
         await player.destroy(`${interaction.user.username} stopped the Player`);
 
