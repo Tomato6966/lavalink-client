18e1da364db0439c2e1176850cdef5843b7c67d6
LucasB25 <50886682+LucasB25@users.noreply.github.com>
2025-04-21 15:32:07 +0200

    Update LavalinkManager.ts

--- src/structures/LavalinkManager.ts
+++ src/structures/LavalinkManager.ts
@@ -514,10 +514,7 @@ export class LavalinkManager extends EventEmitter {
                         message: `Can't send updatePlayer for voice token session - Missing sessionId :: ${JSON.stringify({ voice: { token: update.token, endpoint: update.endpoint, sessionId: sessionId2Use, }, update, playerVoice: player.voice }, null, 2)}`,
                         functionLayer: "LavalinkManager > sendRawData()",
                     });
-                    if (this.options?.advancedOptions?.debugOptions?.noAudio === true) console.debug("Lavalink-Client-Debug | NO-AUDIO [::] sendRawData function, Can't send updatePlayer for voice token session - Missing sessionId", {
-                        voice: { token: update.token, endpoint: update.endpoint, sessionId: sessionId2Use, }, playerVoice: player.voice,
-                        update,
-                    })
+                    if (this.options?.advancedOptions?.debugOptions?.noAudio === true) console.debug("Lavalink-Client-Debug | NO-AUDIO [::] sendRawData function, Can't send updatePlayer for voice token session - Missing sessionId", { voice: { token: update.token, endpoint: update.endpoint, sessionId: sessionId2Use, } });
                 } else {
                     await player.node.updatePlayer({
                         guildId: player.guildId,
