c02f9c2598b33e3a95e157c4738d52780527c7da
Tomato6966 <chris.pre03@gmail.com>
2026-02-27 19:04:26 +0100

    v2.9.4 - fixing build-bundling-issues...

--- tsup.config.mjs
+++ tsup.config.mjs
@@ -10,4 +10,7 @@ export default defineConfig({
     dts: true,
     clean: true,
     bundle: true,
+    outExtension: ({ format }) => ({
+        js: format === "esm" ? ".mjs" : ".js",
+    }),
 });
